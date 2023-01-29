3de1419bd2716ec1cdeeddbd1d41389373f3657c
bryaneriksson <bryaneriksson@uvic.ca>
2023-01-28 18:12:44 -0800

    Carousel working

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
       
       <Routes>
         <Route path='/' element={<Layout/>}>
-          <Route path='/' element={<Home/>}> </Route>
+          <Route path='/' element={<Home movies={movies}/>}> </Route>
         </Route>
       </Routes>
 
